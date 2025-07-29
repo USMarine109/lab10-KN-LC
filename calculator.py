f0f124b1c5f19c9c8cf7de2bce7892530e1b81b7
USMarine109 <kennethnguyen77@gmail.com>
2025-07-29 15:17:17 -0400

    Entering functions

--- calculator.py
+++ calculator.py
@@ -4,8 +4,25 @@ calculator.py
 
 One function per operation, in order.
 """
+import math
 # First example
 def add(a, b): 
+    return a + b
     pass
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return b / a raise ZeroDivisionError if a == 0
+
+def log(a, b):
+    return log(b, a) raise ValueError
+
+def exp(a, b):
+    return exp(a, b)
 
 
