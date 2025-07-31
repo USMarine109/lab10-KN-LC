44ee046f349d78dca934580be2a164c94343269f
Lauren <lcastello@ufl.edu>
2025-07-31 14:16:48 -0400

    Modified calcuator P2

--- calculator.py
+++ calculator.py
@@ -26,3 +26,25 @@ def exp(a, b):
     return exp(a, b)
 
 
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
+
